0de06d1033cb1e3c8213876d4f31c1b9204e72db
chaoyuu <chaoyu995@gmail.com>
2022-08-15 13:01:28 +1000

    g

--- src/App.js
+++ src/App.js
@@ -12,18 +12,18 @@ import Shop from './pages/Shop';
 import BlogDetail from './pages/BlogDetail';
 
 import ProductDetail from './pages/ProductDetail';
-import AboutUs from './pages/AboutUs';
+import AboutUs     from './pages/AboutUs';
 import Blog from './pages/Blog';
 import TeamMember from './components/TeamMember';
 import Contact from './pages/Contact';
 import ShoppingCart from './pages/ShoppingCart';
-import { useState, useEffect } from "react";
+import { useState,
+useEffect } from "react";
 
 import {
 
-  Route,
-  Routes,
- 
+Route,
+Routes,
 } from "react-router-dom";
 
 import * as API from "./APIs/getRouter"
@@ -31,64 +31,59 @@ import * as API from "./APIs/getRouter"
 import axios from "axios"
 
 const Container = styled.div`
- position: relative; 
- background-color: rgba(0,0,0,0.2); 
- background: linear-gradient(180deg, #211D1C 0%, rgba(33, 29, 28, 0) 49.29%); 
+position: relative; 
+background-color: rgba(0,0,0,0.2); 
+background: linear-gradient(180deg, #211D1C 0%, rgba(33, 29, 28, 0) 49.29%); 
 `
 
 function App() {
-  const [stories, setStories] = useState()
-  const [products, setProducts] = useState()
-  
-  useEffect(() => {
-    getStory()
-    getProduct()
-    return () => {
-    }
-  }, [])
+const [stories, setStories] = useState()
+const [products, setProducts] = useState()
+useEffect(() => {
+getStory()
+getProduct()
+return () => {
+}
+}, [])
 
-  var getStory = () => {
-    axios.get("http://localhost:3307/story").then((res, rej) => {
-      if (res) {
-        setStories(res.data)
-      } else {
-        console.log(rej);
-        return
-      }
-    })
-  }
-  var getProduct = () => {
-    axios.get("http://localhost:3307/products").then((res, rej) => {
-      if (res) {
-        setProducts(res.data)
-        //console.log(res);
-      } else {
-        console.log(rej);
-        return
-      }
-    })
-  }
-  return (
-   
-    <Container>
-      <NavBar />
-     
-      <Routes>
-        
-        <Route path="/" element={<Home stories={stories} products={products} />} />
-        <Route path="shop" element={<Shop products={products} />} />
-        <Route path="productdetail" element={<ProductDetail products={products} />} />
-        <Route path="productdetails/:productId" element={<ProductDetail products={products} />} />
-        <Route path="about" element={<AboutUs />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="blog" element={<Blog />} />
-        <Route path="shoppingCart" end element={<ShoppingCart />} />
-        <Route path="blogdetail" element={<BlogDetail />} />
-      </Routes>
-      
-      <Footer />
-    </Container>
-  );
+var getStory = () => {
+axios.get("http://localhost:3307/story").then((res, rej) => {
+if (res) {
+setStories(res.data)
+} else {
+console.log(rej);
+return
+}
+})
+}
+var getProduct = () => {
+axios.get("http://localhost:3307/products").then((res, rej) => {
+if (res) {
+setProducts(res.data)
+//console.log(res);
+} else {
+console.log(rej);
+return
+}
+})
+}
+return (
+<Container>
+<NavBar />
+<Routes>
+<Route path="/" element={<Home stories={stories} products={products} />} />
+<Route path="shop" element={<Shop products={products} />} />
+<Route path="productdetail" element={<ProductDetail products={products} />} />
+<Route path="productdetails/:productId" element={<ProductDetail products={products} />} />
+<Route path="aboutus" element={<AboutUs />} />
+<Route path="contact" element={<Contact />} />
+<Route path="blog" element={<Blog />} />
+<Route path="shoppingCart" end element={<ShoppingCart />} />
+<Route path="blogdetail" element={<BlogDetail />} />
+</Routes>
+<Footer />
+</Container>
+);
 }
 
 export default App;
